dda62eed63c915e39ba0d1625686c4e59a5aa37d
carlosvfb <cacavfb2107@gmail.com>
2025-03-24 21:34:12 -0300

    adicionando o artista

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-
 import { useState } from 'react'
 import './App.css'
+
 function App() {
   const [nomeMusica, setNomeMusica] = useState("")
+  const [artista, setArtista] = useState("")
   const [dadosMusica, setDadosMusica] = useState(null)
   const [erro, setErro] = useState("")
 
   const handleGerarQRCode = async () => {
-    if (!nomeMusica.trim()) {
-      setErro("Por favor, preencha o nome da música")
+    if (!nomeMusica.trim() || !artista.trim()) {
+      setErro("Por favor, preencha o nome da música e o nome do artista.")
       return;
     }
 
     try {
-      const response = await fetch(`https://servidor-hinario.vercel.app/musicas/${encodeURIComponent(nomeMusica)}/qrcode`)
+      const response = await fetch(`https://servidor-hinario.vercel.app/musicas/${encodeURIComponent(nomeMusica)}/${encodeURIComponent(artista)}/qrcode`)
 
       if (!response.ok) {
         throw new Error(`Falha ao gerar QR Code: ${response.status}`)
@@ -31,21 +32,37 @@ function App() {
   return (
     <div>
       <h1>Gerador de QR Code</h1>
-      <div>
-        <input type="text" placeholder="Digite o nome da música" onChange={(e) => setNomeMusica(e.target.value)}/>
+      
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="nomeMusica">Nome da Música: </label>
+        <input 
+          id="nomeMusica"
+          type="text" 
+          placeholder="Digite o nome da música" 
+          onChange={(e) => setNomeMusica(e.target.value)}
+        />
       </div>
+      
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="artista">Nome do Artista: </label>
+        <input 
+          id="artista"
+          type="text" 
+          placeholder="Digite o nome do artista" 
+          onChange={(e) => setArtista(e.target.value)}
+        />
+      </div>
+
       <button onClick={handleGerarQRCode}>Gerar QR Code</button>
 
       {erro && <p>{erro}</p>}
 
       {dadosMusica && (
         <div>
-          
-          <h3>QR Code da Música {dadosMusica.nome}:</h3>
-          <img src={dadosMusica.qrCode} alt={`QR Code de ${dadosMusica.nome}`} />
+          <h3>QR Code da Música {dadosMusica.nome} - {dadosMusica.artista}:</h3>
+          <img src={dadosMusica.qrCode} alt={`QR Code de ${dadosMusica.nome} de ${dadosMusica.artista}`} />
         </div>
       )}
-
     </div>
   )
 }
